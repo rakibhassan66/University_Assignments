5c6a5a7f26278f10aec7da6e1bc52b0801906c9f
Equarius <72136153+rakibhassan66@users.noreply.github.com>
2023-07-30 20:26:14 +0600

    Add files via upload

---
+++ Lab Task -01/myinformation.java
@@ -0,0 +1,5 @@
+public class myinformation {
+    public static void main(String[] args) {
+        System.out.println("Name : Rakib Hassan\nID : 0242220005101554\nAddress : X_12 F2 , Shah-Jahan Road,Mohahammadpur -1207\nDepartment of CSE\nFavourite game name Assassin's Creed");
+    }
+}
