140c47d79fda2319a5ab4d7d95b91afd159a1907
Equarius <72136153+rakibhassan66@users.noreply.github.com>
2024-08-31 21:34:58 +0600

    Delete Lab Task -04 directory

--- Lab Task -04/Source files/string.java
+++
@@ -1,8 +0,0 @@
-public class string {
-    public static void main(String[] args) {
-        String st1 = new String("Hello");
-        String st2 = new String("My");
-        String st3 = new String("Class");
-        System.out.println(st1 + " " + st2 + " " + st3);
-    }
-}
