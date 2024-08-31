42c17e3e2b256f558b3bc6a7eaea905e43c6a38e
Equarius <72136153+rakibhassan66@users.noreply.github.com>
2024-08-31 21:50:36 +0600

    Add files via upload

---
+++ OOP_3_Kotlin/Practice Try 1/src/Main.kt
@@ -0,0 +1,8 @@
+var name = "Equarius"  // Mutable variable
+val age = 25           // Immutable variable
+var count: Int = 10
+val isActive: Boolean = true
+
+fun main() {
+    println("Hello, Kotlin!")
+}
